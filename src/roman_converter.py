08eca4acc076dc0b2f0b0202837da78ba23ca4a3
Arielcohen1827 <arieleitancohen@gmail.com>
2025-04-07 21:02:47 -0300

    test y convertor funcionan con exito

--- src/roman_converter.py
+++ src/roman_converter.py
@@ -1,2 +1,31 @@
 def decimal_to_roman(num):
-    pass
+    
+    if not (1 <= num <= 3999):
+        raise ValueError("Número fuera de rango (debe estar entre 1 y 3999)")
+
+   
+    roman_numerals = [
+        ("M", 1000),
+        ("CM", 900),
+        ("D", 500),
+        ("CD", 400),
+        ("C", 100),
+        ("XC", 90),
+        ("L", 50),
+        ("XL", 40),
+        ("X", 10),
+        ("IX", 9),
+        ("V", 5),
+        ("IV", 4),
+        ("I", 1),
+    ]
+
+    resultado = []
+    for roman, value in roman_numerals:
+        while num >= value:
+            resultado.append(roman)
+            num -= value
+
+    return ''.join(resultado)
+
+
